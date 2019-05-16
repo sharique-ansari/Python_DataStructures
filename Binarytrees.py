a467b70c3f30d608d20c378829afc7bf88b66895
Sharique Ansari <sharique@Khaleeques-MacBook-Pro.local>
2019-05-16 20:04:15 +0530

    added functions to binary tree

--- Binarytrees.py
+++ Binarytrees.py
@@ -7,8 +7,7 @@ class Node:
         self.right = None
         self.left = None
 
-
-    def insert(self,data):
+    def insert(self, data):
         if self.val:
             if self.val < data:
                 if self.right is None:
@@ -30,6 +29,47 @@ class Node:
         if self.right:
             self.right.printtree()
 
+    def findintree(self, fval):
+        if fval < self.val:
+            if self.left is None:
+                return "Given value does not exist in tree:" + str(fval)
+            return self.left.findintree(fval)
+        elif fval > self.val:
+            if self.right is None:
+                return "Given value does not exist in tree:" + str(fval)
+            return self.right.findintree(fval)
+        else:
+            return str(fval) + " is found."
+
+    def minBST(self):
+        if self.left is None:
+            return self
+        return self.left.minBST()
+
+    def delete(self, dval):
+        if self is None:
+            return self
+        if dval < self.val:
+            self.left = self.left.delete()
+        elif dval > self.val:
+            self.right = self.right.delete()
+        else:
+            if self.left is None:
+                temp = self.right
+                self = None
+                return temp
+            elif self.right is None:
+                temp = self.left
+                self = None
+                return temp
+            temp = self.right.minBST()
+            self.val = temp.val
+            self.right = self.right.delete(temp.val)
+
+    def getHeight(self,root):
+        if root is None:
+            return -1
+        return 1 + max(self.getHeight(root.left), self.getHeight(root.right))
 
 
 if __name__ == "__main__":
@@ -40,9 +80,14 @@ if __name__ == "__main__":
     tree.insert(1)
     tree.printtree()
 
+    print(tree.findintree(89))
+    print(tree.findintree(9))
+    print(tree.getHeight(tree))
+
+    # tree.delete(3)
+    # tree.printtree()
+
 #     below is the example tree
 #           3
 #       2         4
 #   1                 9
-
-
